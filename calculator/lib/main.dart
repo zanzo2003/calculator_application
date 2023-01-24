730e21872e7392e84570f820ff4b88979f20cb32
Shashwath Bhaskar <shashwathbhaskar@Shashwaths-MacBook-Pro.local>
2023-01-24 15:39:25 +0530

    base-mbase for the application

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: CalculatorApp(),
+  ));
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorAppState createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
