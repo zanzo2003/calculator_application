dc270d96e52a36ca6bccc688b21f84c70cc17836
Shashwath Bhaskar <shashwathbhaskar@Shashwaths-MacBook-Pro.local>
2023-01-24 23:06:54 +0530

    UI refinement
    few changes and debuged code

--- calculator/lib/ui.dart
+++ calculator/lib/ui.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'colours.dart';
 
-
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({Key? key}) : super(key: key);
 
@@ -10,6 +9,16 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class CalculatorAppState extends State<CalculatorApp> {
+  double firstNumber = 0.0;
+  double secondNumber = 0.0;
+  var input = "";
+  var output = "";
+  var operator = "";
+
+  onButtonClick(value) {
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,67 +27,78 @@ class CalculatorAppState extends State<CalculatorApp> {
         children: [
           Expanded(
               child: Container(
-            color: Colors.red,
+            color: Colors.black,
             width: double.infinity,
-            padding: EdgeInsets.all(22),
+            padding: const EdgeInsets.all(22),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text("Input",
-                style: TextStyle(
-                  fontSize: 48,
-                  color: Colors.white
+                Text(
+                  input,
+                  style: const TextStyle(fontSize: 48, color: Colors.white70),
                 ),
+                const SizedBox(
+                  height: 20,
                 ),
-                Text("Output",
-                style: TextStyle(
-                  fontSize: 48,
-                  color: Colors.white
+                Text(
+                  output,
+                  style: const TextStyle(fontSize: 28, color: Colors.white38),
                 ),
+                const SizedBox(
+                  height: 10,
                 )
               ],
             ),
           )),
           Row(
-            children:[
-              button(text: "AC", buttonColors: operatorColor, textColor: orangecolor),
-              button(text: "\u{2190}",buttonColors: operatorColor,textColor: orangecolor),
+            children: [
+              button(
+                  text: "AC",
+                  buttonColors: operatorColor,
+                  textColor: orangecolor),
+              button(
+                  text: "\u{2190}",
+                  buttonColors: operatorColor,
+                  textColor: orangecolor),
               button(text: "", textColor: Colors.transparent),
-              button(text: "\u{00F7}",buttonColors: operatorColor),
-              ],
+              button(text: "\u{00F7}", buttonColors: operatorColor),
+            ],
           ),
           Row(
-            children:[
+            children: [
               button(text: "7"),
               button(text: "8"),
               button(text: "9"),
-              button(text: "\u{00D7}",buttonColors: buttonColor),
-              ],
+              button(text: "\u{00D7}", buttonColors: buttonColor),
+            ],
           ),
           Row(
-            children:[
+            children: [
               button(text: "4"),
               button(text: "5"),
               button(text: "6"),
-              button(text: "-",buttonColors: buttonColor),
-              ],
+              button(text: "-", buttonColors: buttonColor),
+            ],
           ),
           Row(
-            children:[
+            children: [
               button(text: "1"),
               button(text: "2"),
               button(text: "3"),
-              button(text: "\u{002B}",buttonColors: buttonColor),
-              ],
+              button(text: "\u{002B}", buttonColors: buttonColor),
+            ],
           ),
           Row(
-            children:[
-              button(text: "%",buttonColors: buttonColor,textColor: orangecolor),
+            children: [
+              button(
+                  text: "%", buttonColors: buttonColor, textColor: orangecolor),
               button(text: "0"),
-              button(text: ".",buttonColors: buttonColor,textColor: orangecolor),
-              button(text: "=",buttonColors: buttonColor,textColor: orangecolor),
-              ],
+              button(
+                  text: ".", buttonColors: buttonColor, textColor: orangecolor),
+              button(
+                  text: "=", buttonColors: buttonColor, textColor: orangecolor),
+            ],
           )
         ],
       ),
@@ -91,11 +111,11 @@ class CalculatorAppState extends State<CalculatorApp> {
       margin: const EdgeInsets.all(8),
       child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-              shape: CircleBorder(),
+              shape: const CircleBorder(),
               padding: const EdgeInsets.all(22),
               backgroundColor: buttonColors),
-          onPressed: () {},
-          child:Text(
+          onPressed: () => onButtonClick(text),
+          child: Text(
             text,
             style: TextStyle(
                 fontSize: 18, color: textColor, fontWeight: FontWeight.bold),
